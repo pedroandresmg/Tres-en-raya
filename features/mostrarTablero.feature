0eccecbfc60f26ac9dc5493aee19ebb9373fcda0
Pedro <pedroandresmg@gmail.com>
2017-12-22 10:22:47 -0400

    primer boton para el feature mostrarTablero implementado

---
+++ features/mostrarTablero.feature
@@ -0,0 +1,7 @@
+Feature:    Como jugador
+            Quiero mostrar tablero
+            Para marcar mis jugadas
+
+Scenario:   
+            Given visito la pagina del juego
+            Then deberia mostrar un boton con caracter "-"
--- features/step_definitions/tresEnRaya_steps.rb
+++ features/step_definitions/tresEnRaya_steps.rb
@@ -5,3 +5,7 @@ Given("visito la pagina del juego") do
   Then("deberia mostrar {string}") do |string|
     last_response.body.should =~ /#{string}/m
   end
+
+  Then("deberia mostrar un boton con caracter {string}") do |string|
+    last_response.body.should =~ /#{string}/m
+  end
